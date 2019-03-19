779dde848635f3cb4447853a0d607443d796f9c6
billdaff <billdafeldecker@gmail.com>
2019-03-19 12:22:32 -0400

    update google maps

--- js/main.js
+++ js/main.js
@@ -272,7 +272,7 @@
         //Google Maps
         $("#map").googleMap();
         $("#map").addMarker({
-            address: "15 avenue des champs Elysées 75008 Paris" // Your Address
+            address: "Brick NJ, 08723" // Your Address
         });
     });
 
