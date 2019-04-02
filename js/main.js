9d1696fc030776c9c452d8b7e5edd31c2fae7c44
billdaff <billdafeldecker@gmail.com>
2019-04-02 13:01:25 -0400

    update portfolio and general stuff

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@
 * Version: 1.0
 */
 
-(function($) {
-"use strict";
+(function ($) {
+    "use strict";
     // Subpages resize
     function subpages_resize() {
         var subpagesHeight = $('.pt-page-current').height();
         $(".subpages").height(subpagesHeight + 50);
     }
-    
+
     // Portfolio subpage filters
     function portfolio_init() {
         var portfolio_grid = $('#portfolio_grid'),
             portfolio_filter = $('#portfolio_filters');
-            
+
         if (portfolio_grid) {
 
             portfolio_grid.shuffle({
@@ -35,47 +35,14 @@
                 e.preventDefault();
                 $('#portfolio_filters .filter').parent().removeClass('active');
                 $(this).parent().addClass('active');
-                portfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
-                setTimeout(function(){
+                portfolio_grid.shuffle('shuffle', $(this).attr('data-group'));
+                setTimeout(function () {
                     subpages_resize();
                 }, 500);
             });
 
         }
     }
-    // /Portfolio subpage filters
-
-    // Contact form validator
-    $(function () {
-
-        $('#contact-form').validator();
-
-        $('#contact-form').on('submit', function (e) {
-            if (!e.isDefaultPrevented()) {
-                var url = "contact_form/contact_form.php";
-
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: $(this).serialize(),
-                    success: function (data)
-                    {
-                        var messageAlert = 'alert-' + data.type;
-                        var messageText = data.message;
-
-                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                        if (messageAlert && messageText) {
-                            $('#contact-form').find('.messages').html(alertBox);
-                            $('#contact-form')[0].reset();
-                        }
-                    }
-                });
-                return false;
-            }
-        });
-    });
-    // /Contact form validator
-
     // Hide Mobile menu
     function mobileMenuHide() {
         var windowWidth = $(window).width();
@@ -87,7 +54,7 @@
 
     //On Window load & Resize
     $(window)
-        .on('load', function() { //Load
+        .on('load', function () { //Load
             // Animation on Page Loading
             $(".preloader").fadeOut("slow");
 
@@ -99,10 +66,10 @@
                 });
             }
         })
-        .on('resize', function() { //Resize
-             mobileMenuHide();
+        .on('resize', function () { //Resize
+            mobileMenuHide();
 
-             setTimeout(function(){
+            setTimeout(function () {
                 subpages_resize();
             }, 500);
         })
@@ -117,21 +84,21 @@
 
 
     // On Document Load
-    $(document).on('ready', function() {
+    $(document).on('ready', function () {
         // Initialize Portfolio grid
         var $portfolio_container = $("#portfolio-grid");
 
         $portfolio_container.imagesLoaded(function () {
-            setTimeout(function(){
+            setTimeout(function () {
                 portfolio_init(this);
             }, 500);
         });
 
         // Portfolio hover effect init
-        $(' #portfolio_grid > figure ').each( function() { $(this).hoverdir(); } );
+        $(' #portfolio_grid > figure ').each(function () { $(this).hoverdir(); });
 
         // Blog grid init
-        setTimeout(function(){
+        setTimeout(function () {
             var $container = $(".blog-masonry");
             $container.masonry();
         }, 500);
@@ -158,20 +125,20 @@
             loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
             navText: false,
             margin: 25,
-            responsive : {
+            responsive: {
                 // breakpoint from 0 up
-                0 : {
+                0: {
                     items: 1,
                 },
                 // breakpoint from 480 up
-                480 : {
+                480: {
                     items: 1,
                 },
                 // breakpoint from 768 up
-                768 : {
+                768: {
                     items: 2,
                 },
-                1200 : {
+                1200: {
                     items: 2,
                 }
             }
@@ -210,31 +177,31 @@
             },
 
             iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                      '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '<div class="mfp-title mfp-bottom-iframe-title"></div>' +
+                    '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 
                 patterns: {
                     youtube: {
-                      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+                        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-                      id: null, // String that splits URL in a two parts, second part should be %id%
-                      // Or null - full URL will be returned
-                      // Or a function that should return %id%, for example:
-                      // id: function(url) { return 'parsed id'; }
+                        id: null, // String that splits URL in a two parts, second part should be %id%
+                        // Or null - full URL will be returned
+                        // Or a function that should return %id%, for example:
+                        // id: function(url) { return 'parsed id'; }
 
-                      src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
+                        src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
                     },
                     vimeo: {
-                      index: 'vimeo.com/',
-                      id: '/',
-                      src: '//player.vimeo.com/video/%id%?autoplay=1'
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
                     },
                     gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
                     }
                 },
 
@@ -242,8 +209,8 @@
             },
 
             callbacks: {
-                markupParse: function(template, values, item) {
-                 values.title = item.el.attr('title');
+                markupParse: function (template, values, item) {
+                    values.title = item.el.attr('title');
                 }
             },
         });
@@ -260,20 +227,17 @@
         //Form Controls
         $('.form-control')
             .val('')
-            .on("focusin", function(){
+            .on("focusin", function () {
                 $(this).parent('.form-group').addClass('form-group-focus');
             })
-            .on("focusout", function(){
-                if($(this).val().length === 0) {
+            .on("focusout", function () {
+                if ($(this).val().length === 0) {
                     $(this).parent('.form-group').removeClass('form-group-focus');
                 }
             });
 
-        //Google Maps
-        $("#map").googleMap();
-        $("#map").addMarker({
-            address: "Brick, NJ" // Your Address
-        });
+
+
     });
 
 })(jQuery);
